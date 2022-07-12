9bee84142fa17fa5bba77aad8465a57222db9268
Genadi Mihailov <108881802+genadi03@users.noreply.github.com>
2022-07-12 11:44:52 +0300

    Add files via upload

---
+++ concatenateDate.js
@@ -0,0 +1,8 @@
+function concatenateDate(){
+    let firstName = "Maria";
+    let lastName = "Ivanova";
+    let age = 20;
+    let town = "Pleven";
+    console.log(`You are ${firstName} ${lastName}, a ${age}-years old person from ${town}.`);
+}
+concatenateDate('Maria', 'Ivanova', 19, 'Pleven')
